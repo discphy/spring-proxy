a94b64706eee1f8071423d87fc9b6a65334579d3
discphy <dinophy@nate.com>
2023-10-11 08:32:17 +0900

    feat(add): Proxy factory 2 added

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceInterface;
 import hello.proxy.common.service.ServiceInterfaceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -30,5 +31,41 @@ public class ProxyFactoryTest {
 		assertThat(AopUtils.isAopProxy(proxy)).isTrue();
 		assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
 		assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+	}
+
+	@Test
+	@DisplayName("구체 클래스만 있으면 CGLIB 사용")
+	void concreteProxy() {
+		ConcreteService target = new ConcreteService();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+
+		proxyFactory.addAdvice(new TimeAdvice());
+		ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+		log.info("targetClass={}", target.getClass());
+		log.info("proxyClass={}", proxy.getClass());
+
+		proxy.call();
+
+		assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+		assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+		assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+	}
+
+	@Test
+	@DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+	void proxyTargetClass() {
+		ServiceInterfaceImpl target = new ServiceInterfaceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		proxyFactory.setProxyTargetClass(true);
+		proxyFactory.addAdvice(new TimeAdvice());
+		ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+		log.info("targetClass={}", target.getClass());
+		log.info("proxyClass={}", proxy.getClass());
+
+		proxy.save();
+
+		assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+		assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+		assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
 	}
 }
