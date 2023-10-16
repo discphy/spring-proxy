4d97cabd0b7a58533f81992af9ce136d239d6485
discphy <dinophy@nate.com>
2023-10-17 08:50:42 +0900

    feat(add): Spring Bean postprocessor added

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,28 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+	@Bean
+	public Advisor advisor1(LogTrace trace) {
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedNames("request*", "order*", "save*");
+
+		LogTraceAdvice advice = new LogTraceAdvice(trace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+}
