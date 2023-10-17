3c917ff652d89de8e96484e76702e079f0cec73e
discphy <dinophy@nate.com>
2023-10-18 08:19:47 +0900

    feat(add): Spring Bean postprocessor added

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,28 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-	@Bean
+//	@Bean
 	public Advisor advisor1(LogTrace trace) {
 		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
 		pointcut.setMappedNames("request*", "order*", "save*");
+
+		LogTraceAdvice advice = new LogTraceAdvice(trace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+//	@Bean
+	public Advisor advisor2(LogTrace trace) {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+
+		LogTraceAdvice advice = new LogTraceAdvice(trace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+	@Bean
+	public Advisor advisor3(LogTrace trace) {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 
 		LogTraceAdvice advice = new LogTraceAdvice(trace);
 		return new DefaultPointcutAdvisor(pointcut, advice);
